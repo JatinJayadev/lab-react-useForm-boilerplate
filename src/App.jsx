010f573ca8567481f502cf48610a131e991530f7
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-28 20:48:44 +0530

    Done jsx!

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,92 @@
-
-import { useForm } from 'react-hook-form'
-import './App.css'
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 
 function App() {
-  const form = useForm()
+  const [state, setState] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control
+  } = useForm();
 
+  const SubmitHandler = (data) => {
+    console.log(data);
+    setState(true);
+  };
   return (
-    <>
-      <form>
-        <label htmlFor="username">UserName</label>
-        <input type="text" id='username' />
+    <div>
 
-        <label htmlFor="email">Email</label>
-        <input type="email" id='email' />
+      <form onSubmit={handleSubmit(SubmitHandler)}>
+        <div>{state ? 'Registration Successfull' : ''}</div>
 
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id='channel' />
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          placeholder="First Name.."
+          {...register('firstName', {
+            required: 'Please dont keep input feild empty',
+          })}
+        />
 
-        <button>Submit</button>
-      </form>
-    </>
-  )
+
+        {errors.firstName ? (<p>{errors.firstName.message}</p>) : ""}
+
+        <label htmlFor="">Last Name</label>
+        <input
+          // required={true}
+          type="text"
+          placeholder="Last Name.."
+          {...register('lastName', {
+            required: 'Please dont keep input feild empty',
+          })}
+        />
+
+        {errors.lastName ? (<p>{errors.lastName.message}</p>) : ""}
+
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          placeholder="Email.."
+          {...register('email', {
+            required: "Please don't keep input field empty",
+            validate: (value) => {
+              if (value.includes('@')) {
+                return true; // Valid email
+              }
+              return 'invalid email';
+            },
+          })}
+
+        />
+        {errors.email ? (<p>{errors.email.message}</p>) : ""}
+
+        <label htmlFor="">Password</label>
+        <input
+          // required={true}
+          type="password"
+          placeholder="Password.."
+          {...register('password', {
+            required: "Please don't keep input field empty",
+            minLength: {
+              value: 5,
+              message: 'Password must be more than 4 characters',
+            },
+            maxLength: {
+              value: 20,
+              message: 'Password cannot be more than 20 characters',
+            },
+          })}
+        />
+
+        {errors.password ? (<p>{errors.password.message}</p>) : ""}
+
+        <input type="submit" value={'Register'} />
+      </form >
+      <DevTool control={control} />
+    </div >
+  );
 }
 
-export default App
+export default App;
